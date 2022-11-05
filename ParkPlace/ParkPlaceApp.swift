3b79f49143657987683018f5403d5c00a39715b7
snehith01001110 <snehith@ucsb.edu>
2022-11-05 14:04:49 -0700

    Initial Commit

---
+++ ParkPlace/ParkPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkPlaceApp.swift
+//  ParkPlace
+//
+//  Created by Snehith Nayak on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ParkPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
